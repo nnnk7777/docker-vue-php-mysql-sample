24462302f8d8110ba8d5c4b5d07b7241405f6775
nonaka <electro0701@gmail.com>
2021-07-09 11:40:36 +0900

    update PDO Connection

--- api/src/getAnswers.php
+++ api/src/getAnswers.php
@@ -5,41 +5,30 @@
   header('Content-Type: application/json; charset=utf-8');
 
 
-  // $secret = file_get_contents("../.env");
-  // $params = explode("\n", $secret);
-  // define("DSN", $params[0]);
-  // define("USERNAME", $params[1]);
-  // define("PASSWORD", $params[2]);
-  //   // mysql:host=localhost;dbname=fork_playlist;charset=utf8
-  //   // nakamura-lab
-  //   // n1k2m3r4fms
-  // define("OPTIONS", [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4']);
+  define("DSN", "mysql:host=mysql:3306;dbname=".getenv('MYSQL_DATABASE'));
+  define("USERNAME", getenv('MYSQL_USER'));
+  define("PASSWORD", getenv('MYSQL_PASSWORD'));
+  define("OPTIONS", [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4']);
 
 
+  try {
+    $connection = new PDO(DSN, USERNAME, PASSWORD);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  //   try {
-  //       $pdo = new PDO(DSN, USERNAME, PASSWORD);
-  //       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //   } catch (PDOException $e) {
-  //       exit($e->GETMessage());
-  //   }
+    $q = "SELECT * FROM `answers`";
 
+    $stmt = $connection->query($q);
+    $result_json = array();
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $temp= array('id'  => $result['id'],
+                      'uid'    => $result['uid'],
+                      'qid'    => $result['qid'],
+                      'answer'    => $result['answer']);
+        array_push($result_json, $temp);
+    }
+    echo json_encode($result_json);
+    $connection = null;
 
-
-  //   $answer_list = [];
-  //   $sql = "SELECT * FROM `answer_table`";
-  //     $stmt = $pdo -> prepare($sql);
-  //     $stmt->execute();
-  //     foreach ($stmt as $row) {
-  //       $answer_list[] = [
-  //           'id'       => $row['id'],
-  //           'uid'      => $row['uid'],
-  //           'qid'      => $row['qid'],
-  //           'answer'      => $row['answer'],
-  //       ];
-  //     }
-
-  //   echo json_encode($answer_list);
-
-  echo "Hello, PHP!"
- ?>
+  } catch (PDOException $e) {
+      echo '[ Connection failed ] <br/>' .$e->getMessage();
+  }
